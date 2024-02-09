c73ce381c3f42383a346bb811d4fabfec1ba1a71
Ehab Borae <68748080+ehaborae@users.noreply.github.com>
2024-02-09 14:35:10 +0200

    A start point.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:study_routing/screens/use_navigator/start_point.dart';
+import 'package:study_routing/features/use_navigator/start_point.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
